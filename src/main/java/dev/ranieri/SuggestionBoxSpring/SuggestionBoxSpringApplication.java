e02c4a16e509d3ee4f6b02bb6bc8f7cb06bb3485
AdamRanieri <adaman94@gmail.com>
2022-05-10 11:38:15 -0400

    Initial commit

---
+++ src/main/java/dev/ranieri/SuggestionBoxSpring/SuggestionBoxSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.ranieri.SuggestionBoxSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggestionBoxSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuggestionBoxSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ranieri/SuggestionBoxSpring/SuggestionBoxSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ranieri.SuggestionBoxSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuggestionBoxSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
